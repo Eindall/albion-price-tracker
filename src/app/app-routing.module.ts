2854ef1ab9a19752390660da2862f9aed2bf9e24
Thomas Marquis <thomas.marquis@epitech.eu>
2019-07-12 15:57:47 +0200

    Added Searchbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearcherComponent } from './searcher/searcher.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'search', component: SearcherComponent},
+  {path: '', redirectTo: '/search', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
